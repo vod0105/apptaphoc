c6f5ba20f2a7f519772706319d77f2db76629971
vod0105 <102397278+vod0105@users.noreply.github.com>
2025-09-12 22:10:58 +0700

    update time calendar

---
+++ src/components/ModalAddCalendar.js
@@ -0,0 +1,99 @@
+// ModalAddCalendar.js
+import React from "react";
+
+export default function ModalAddCalendar({
+    newEvent,
+    setNewEvent,
+    handleSave,
+    setModalOpen
+}) {
+    return (
+        <div className="modal-overlay">
+            <div className="modal">
+                <h3>Thêm sự kiện</h3>
+                <label>
+                    Tên sự kiện:
+                    <input
+                        type="text"
+                        placeholder="Tên"
+                        value={newEvent.title}
+                        onChange={(e) =>
+                            setNewEvent({ ...newEvent, title: e.target.value })
+                        }
+                    />
+                </label>
+
+                <label>
+                    Bắt đầu:
+                    <input
+                        type="datetime"
+                        value={newEvent.start}
+                        onChange={(e) =>
+                            setNewEvent({ ...newEvent, start: e.target.value })
+                        }
+                    />
+                </label>
+
+                <label>
+                    Kết thúc:
+                    <input
+                        type="datetime"
+                        value={newEvent.end}
+                        onChange={(e) =>
+                            setNewEvent({ ...newEvent, end: e.target.value })
+                        }
+                    />
+                </label>
+
+                <label>
+                    Kiểu lặp:
+                    <select
+                        value={newEvent.repeat}
+                        onChange={(e) =>
+                            setNewEvent({ ...newEvent, repeat: e.target.value })
+                        }
+                    >
+                        <option value="none">Không lặp</option>
+                        <option value="daily">Hằng ngày</option>
+                        <option value="weekly">Hằng tuần</option>
+                    </select>
+                </label>
+
+                {newEvent.repeat === "weekly" && (
+                    <div className="days-of-week">
+                        Chọn:
+                        {[0, 1, 2, 3, 4, 5, 6].map((d) => (
+                            <label key={d}>
+                                <input
+                                    type="checkbox"
+                                    checked={newEvent.daysOfWeek.includes(d)}
+                                    onChange={(e) => {
+                                        if (e.target.checked) {
+                                            setNewEvent({
+                                                ...newEvent,
+                                                daysOfWeek: [...newEvent.daysOfWeek, d],
+                                            });
+                                        } else {
+                                            setNewEvent({
+                                                ...newEvent,
+                                                daysOfWeek: newEvent.daysOfWeek.filter(
+                                                    (day) => day !== d
+                                                ),
+                                            });
+                                        }
+                                    }}
+                                />
+                                {["CN", "T2", "T3", "T4", "T5", "T6", "T7"][d]}
+                            </label>
+                        ))}
+                    </div>
+                )}
+
+                <div className="modal-buttons">
+                    <button onClick={handleSave}>Lưu</button>
+                    <button onClick={() => setModalOpen(false)}>Hủy</button>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/MyStudyCalendar.js
+++ src/pages/MyStudyCalendar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, use, act } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
@@ -6,11 +6,13 @@ import interactionPlugin from "@fullcalendar/interaction";
 import "../styles/MyStudyCalendar.css";
 import { db, auth } from "../firebase";
 import { collection, setDoc, query, doc, getDocs, where, Timestamp, deleteDoc } from "firebase/firestore";
-
+import ModalAddCalendar from "../components/ModalAddCalendar";
+import ModalUpdateCalendar from "../components/ModalUpdateCalender";
 
 
 export default function MyStudyCalendar({ events }) {
     const [modalOpen, setModalOpen] = useState(false);
+    const [modalUpdateOpen, setModalUpdateOpen] = useState(false);
     const [newEvent, setNewEvent] = useState({
         title: "",
         repeat: "none", // none | daily | weekly
@@ -21,6 +23,8 @@ export default function MyStudyCalendar({ events }) {
     const [calendarApi, setCalendarApi] = useState(null);
 
     const [sessions, setSessions] = useState([]);
+    const [selectedEvent, setSelectedEvent] = useState(null);
+
 
     useEffect(() => {
         const fetchSessions = async () => {
@@ -35,11 +39,11 @@ export default function MyStudyCalendar({ events }) {
                     where("createdAt", ">=", Timestamp.fromDate(thirtyDaysAgo))
                 );
 
-                // Query 2: tất cả weekly repeat
+                // Query 2: tất cả weekly and daily repeat
                 const q2 = query(
                     collection(db, "studySessions"),
                     where("userId", "==", auth.currentUser.uid),
-                    where("repeat", "==", "weekly")
+                    where("repeat", "in", ["weekly", "daily"])
                 );
 
                 const [snap1, snap2] = await Promise.all([getDocs(q1), getDocs(q2)]);
@@ -49,10 +53,10 @@ export default function MyStudyCalendar({ events }) {
                 querySnapshot.forEach((doc) => {
                     const data = doc.data();
 
-                    if (data.repeat === "weekly" && data.daysOfWeek) {
+                    if ((data.repeat === "weekly" || data.repeat === "daily") && data.daysOfWeek) {
+                        console.log("Xử lý sự kiện lặp:", data.job);
                         // generate các lần lặp trong 30 ngày tới
                         const current = new Date(thirtyDaysAgo);
-                        console.log("day: ", data);
                         while (current <= thirtyDaysLater) {
                             if (data.daysOfWeek.includes(current.getDay())) {
                                 // start
@@ -64,12 +68,16 @@ export default function MyStudyCalendar({ events }) {
                                 const [h2, m2] = data.endDate.split(":");
                                 const end = new Date(current);
                                 end.setHours(Number(h2), Number(m2), 0, 0);
-
+                                console.log("Lặp lại sự kiện:", data.job, "vào", start);
                                 allEvents.push({
                                     id: `${doc.id}-${start.getTime()}`,
                                     title: data.job,
                                     start,
                                     end,
+                                    extendedProps: {
+                                        repeat: data.repeat,
+                                        daysOfWeek: data.daysOfWeek,
+                                    }
                                 });
                             }
 
@@ -98,7 +106,6 @@ export default function MyStudyCalendar({ events }) {
                                 end,
                             });
                         }
-
                     }
                 });
 
@@ -108,10 +115,6 @@ export default function MyStudyCalendar({ events }) {
 
         fetchSessions();
     }, []);
-
-    useEffect(() => {
-        console.log("Sessions updated:", sessions);
-    }, [sessions]);
 
     const handleSelect = (info) => {
         setCalendarApi(info.view.calendar);
@@ -135,6 +138,7 @@ export default function MyStudyCalendar({ events }) {
                 daysOfWeek: newEvent.daysOfWeek, // mảng số [1,3,5]
                 startTime: newEvent.start.split("T")[1],
                 endTime: newEvent.end.split("T")[1],
+                extendedProps: { repeat: "weekly", daysOfWeek: newEvent.daysOfWeek },
             });
             try {
                 const docRef = doc(collection(db, "studySessions"));
@@ -155,15 +159,35 @@ export default function MyStudyCalendar({ events }) {
             }
 
         } else if (newEvent.repeat === "daily") {
+            const eventDate = new Date(newEvent.start);
+            const dayOfWeek = eventDate.getDay(); // 0 = CN, 1 = T2, ..., 6 = T7
             // lặp lại hằng ngày
             calendarApi.addEvent({
                 title: newEvent.title,
-                startRecur: newEvent.start,
-                endRecur: newEvent.end,
-                daysOfWeek: [0, 1, 2, 3, 4, 5, 6],
+                daysOfWeek: [dayOfWeek],
                 startTime: newEvent.start.split("T")[1],
                 endTime: newEvent.end.split("T")[1],
+                extendedProps: { repeat: "daily", daysOfWeek: [dayOfWeek] },
             });
+
+            try {
+                const docRef = doc(collection(db, "studySessions"));
+                const sessionData = {
+                    sessionId: docRef.id,
+                    userId: auth.currentUser.uid,
+                    endDate: newEvent.end.split("T")[1],
+                    job: newEvent.title,
+                    repeat: "daily",
+                    daysOfWeek: [dayOfWeek],
+                    startDate: newEvent.start.split("T")[1],
+                };
+                console.log("Session Data to be saved:", sessionData);
+                await setDoc(docRef, sessionData);
+                console.log("Document created with ID:", docRef.id, "Data:", sessionData);
+            } catch (error) {
+                console.error("Lỗi khi tạo phiên học:", error);
+            }
+
         } else {
             const startDate = new Date(newEvent.start);
             const endDate = new Date(newEvent.end);
@@ -200,30 +224,135 @@ export default function MyStudyCalendar({ events }) {
         setModalOpen(false);
     };
 
-    const handleDelete = async (info) => {
-        const confirmDelete = window.confirm(`Xóa sự kiện '${info.event.title}'?`);
-        if (confirmDelete) {
-            try {
-                // 1. Xóa trên Firestore (nếu có id)
-                if (info.event.id) {
-                    await deleteDoc(doc(db, "studySessions", info.event.id.split("-")[0]));
+    const handleUpdate = async () => {
+        if (!selectedEvent) return;
+
+        try {
+            // 2. Update Firestore
+            if (newEvent.repeat === "weekly" && (!newEvent.daysOfWeek || newEvent.daysOfWeek.length === 0)) {
+                alert("Vui lòng chọn ngày trong tuần cho lịch lặp lại hàng tuần.");
+                return;
+            }
+
+            if (newEvent.repeat === "weekly") {
+                try {
+                    selectedEvent.setProp("title", newEvent.title);
+                    selectedEvent.setStart(newEvent.start.split("T")[1]);
+                    selectedEvent.setEnd(newEvent.end.split("T")[1]);
+                    selectedEvent.setExtendedProp("repeat", "weekly");
+                    selectedEvent.setExtendedProp("daysOfWeek", newEvent.daysOfWeek);
+                    await setDoc(
+                        doc(db, "studySessions", selectedEvent.id.split("-")[0]),
+                        {
+                            job: newEvent.title,
+                            startDate: newEvent.start.split("T")[1],
+                            endDate: newEvent.end.split("T")[1],
+                            repeat: "weekly",
+                            daysOfWeek: newEvent.daysOfWeek,
+                        },
+                        { merge: true }
+                    );
+                } catch (error) {
+                    console.error("Lỗi khi update:", error);
                 }
 
-                // 2. Xóa khỏi calendar UI
-                info.event.remove();
-
-                // 3. Cập nhật state sessions
-                setSessions((prev) =>
-                    prev.filter((ev) => ev.id !== info.event.id)
-                );
-
-                console.log("Đã xóa:", info.event.id);
-            } catch (error) {
-                console.error("Lỗi khi xóa:", error);
             }
+            else if (newEvent.repeat === "daily") {
+                const eventDate = new Date(newEvent.start);
+                const dayOfWeek = eventDate.getDay(); // 0 = CN, 1 = T2, ..., 6 = T7
+                try {
+                    selectedEvent.setProp("title", newEvent.title);
+                    selectedEvent.setStart(newEvent.start.split("T")[1]);
+                    selectedEvent.setEnd(newEvent.end.split("T")[1]);
+                    selectedEvent.setExtendedProp("repeat", "daily");
+                    selectedEvent.setExtendedProp("daysOfWeek", [dayOfWeek]);
+                    
+                    await setDoc(
+                        doc(db, "studySessions", selectedEvent.id.split("-")[0]),
+                        {
+                            job: newEvent.title,
+                            startDate: newEvent.start.split("T")[1],
+                            endDate: newEvent.end.split("T")[1],
+                            repeat: "daily",
+                            daysOfWeek: [dayOfWeek],
+                        },
+                        { merge: true }
+                    );
+                } catch (error) {
+                    console.error("Lỗi khi update:", error);
+                }
+            } else {
+                const startDate = new Date(newEvent.start);
+                const endDate = new Date(newEvent.end);
+                const initialMinutes = Math.floor((endDate - startDate) / (1000 * 60)); // số phút phải học
+
+                selectedEvent.setProp("title", newEvent.title);
+                selectedEvent.setStart(startDate);
+                selectedEvent.setEnd(endDate);
+
+                await setDoc(
+                    doc(db, "studySessions", selectedEvent.id.split("-")[0]),
+                    {
+                        job: newEvent.title,
+                        end: new Date(newEvent.end),
+                        updatedAt: new Date(),
+                        totalFocusMinutes: initialMinutes,
+                        remainingMinutes: initialMinutes, // reset lại remaining
+                        actualFocusMinutes: 0,
+                        createdAt: startDate
+                    },
+                    { merge: true }
+                );
+            }
+
+            console.log("Đã update:", selectedEvent.id);
+        } catch (error) {
+            console.error("Lỗi khi update:", error);
         }
+
+        setModalUpdateOpen(false);
+    };
+
+    const handleDelete = async (info) => {
+        try {
+            // 1. Xóa trên Firestore (nếu có id)
+            if (info.event.id) {
+                await deleteDoc(doc(db, "studySessions", info.event.id.split("-")[0]));
+            }
+
+            // 2. Xóa khỏi calendar UI
+            info.event.remove();
+
+            // 3. Cập nhật state sessions
+            setSessions((prev) =>
+                prev.filter((ev) => ev.id !== info.event.id)
+            );
+
+            console.log("Đã xóa:", info.event.id);
+        } catch (error) {
+            console.error("Lỗi khi xóa:", error);
+        }
+        setModalUpdateOpen(false);
     }
 
+    const handleClick = (info) => {
+        console.log("Event clicked:", info.event);
+
+        // ✅ lưu lại event đang chọn để truyền sang modal update
+        setSelectedEvent(info.event);
+        setNewEvent({
+            title: info.event.title,
+            start: info.event.startStr,
+            end: info.event.endStr,
+            repeat: info.event.extendedProps.repeat || "none",
+            daysOfWeek: info.event.extendedProps.daysOfWeek || [],
+        });
+        setModalUpdateOpen(true);
+    };
+
+    useEffect(() => {
+        console.log("sessions updated:", sessions);
+    }, [sessions]);
     return (
         <>
             <FullCalendar
@@ -237,71 +366,25 @@ export default function MyStudyCalendar({ events }) {
                 selectable={true}
                 select={handleSelect}
                 events={sessions}
-                eventClick={handleDelete}
+                eventClick={handleClick}
             />
+            {modalUpdateOpen && (
+                <ModalUpdateCalendar
+                    newEvent={newEvent}
+                    setNewEvent={setNewEvent}
+                    handleUpdate={handleUpdate}
+                    handleDelete={() => handleDelete({ event: selectedEvent })}
+                    setModalUpdateOpen={setModalUpdateOpen}
+                />
+            )}
 
             {modalOpen && (
-                <div className="modal-overlay">
-                    <div className="modal">
-                        <h3>Thêm sự kiện</h3>
-                        <input
-                            type="text"
-                            placeholder="Tên sự kiện"
-                            value={newEvent.title}
-                            onChange={(e) =>
-                                setNewEvent({ ...newEvent, title: e.target.value })
-                            }
-                        />
-
-                        <label>
-                            Kiểu lặp:
-                            <select
-                                value={newEvent.repeat}
-                                onChange={(e) =>
-                                    setNewEvent({ ...newEvent, repeat: e.target.value })
-                                }
-                            >
-                                <option value="none">Không lặp</option>
-                                <option value="daily">Hằng ngày</option>
-                                <option value="weekly">Hằng tuần</option>
-                            </select>
-                        </label>
-
-                        {newEvent.repeat === "weekly" && (
-                            <div className="days-of-week">
-                                Chọn thứ:
-                                {[0, 1, 2, 3, 4, 5, 6].map((d) => (
-                                    <label key={d}>
-                                        <input
-                                            type="checkbox"
-                                            checked={newEvent.daysOfWeek.includes(d)}
-                                            onChange={(e) => {
-                                                if (e.target.checked) {
-                                                    setNewEvent({
-                                                        ...newEvent,
-                                                        daysOfWeek: [...newEvent.daysOfWeek, d],
-                                                    });
-                                                } else {
-                                                    setNewEvent({
-                                                        ...newEvent,
-                                                        daysOfWeek: newEvent.daysOfWeek.filter(
-                                                            (day) => day !== d
-                                                        ),
-                                                    });
-                                                }
-                                            }}
-                                        />
-                                        {["CN", "T2", "T3", "T4", "T5", "T6", "T7"][d]}
-                                    </label>
-                                ))}
-                            </div>
-                        )}
-                        <div className="modal-buttons">
-                            <button onClick={handleSave}>Lưu</button>
-                            <button onClick={() => setModalOpen(false)}>Hủy</button>
-                        </div>
-                    </div>
-                </div>
+                <ModalAddCalendar
+                    newEvent={newEvent}
+                    setNewEvent={setNewEvent}
+                    handleSave={handleSave}
+                    setModalOpen={setModalOpen}
+                />
             )}
         </>
     );
